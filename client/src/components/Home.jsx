9e0368f45d830bf4002b89bf2e35e50a3472de1e
Jordan <jordan.olaves91@gmail.com>
2021-08-13 22:22:21 -0500

    PIVG

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
 import { getVideogames } from '../actions';
 import Card from './Card';
+import Pagination from './Pagination'
 
 export default function HomePage(){
     const dispatch = useDispatch()
 
     const allVideogames = useSelector((state) => state.videogames)
+    const [currentPage, setCurrentPage] = useState(1);
+    const [videogamesPerPage, setVideogamesPerPage] = useState(9);
+    const lastGameIndex = currentPage * videogamesPerPage;
+    const firstGameIndex = lastGameIndex - videogamesPerPage;
+    const currentVideogames = allVideogames.slice(firstGameIndex, lastGameIndex);
+
+    const paginado = (pageNumber) =>{
+        setCurrentPage(pageNumber)
+    }
 
     useEffect (() => {
         dispatch(getVideogames())
@@ -32,11 +42,15 @@ export default function HomePage(){
                     <option value="desc">Descendente</option>
                 </select>
                 <select>
-                    <option value="">Todos</option>
                     <option value="">Existentes</option>
                     <option value="">Creados</option>
                 </select>
-                {allVideogames?.map((e) => {
+                <Pagination
+                videogamesPerPage = {videogamesPerPage}
+                allVideogames = {allVideogames.length}
+                paginado = {paginado}
+                />
+                {currentVideogames?.map((e) => {
                     return (
                         <fragment>
                             <Link to={"/home/" + e.id}>
